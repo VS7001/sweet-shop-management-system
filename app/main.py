30cd232a1ef3524ca16cb0474365e316bfae873d
Vivek Suryawanshi <vivekss27004@gmail.com>
2025-12-13 22:54:20 +0530

    feat: add basic user registration endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
 
 @app.get("/")
 def root():
     return {"message": "Sweet Shop Backend is running"}
+
+@app.post("/api/auth/register", status_code=201)
+def register_user(data: RegisterRequest):
+    return {
+        "email": data.email
+    }
